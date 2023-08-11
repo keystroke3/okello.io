7cae090df75ee4a088e96e765b29a97488ddb984
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2023-08-12 00:02:16 +0300

    Add Sala and blog to the page; Change look and feel

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,52 @@
   <div class="main">
     <img src="./assets/okello.svg" alt="logo" />
     <h1> Hola, I am Ted</h1>
-    <h2>My Tools</h2>
+    <p>A passionate software tools developer from Kenya. I build everything from automation scripts to APIs. I mainly work
+      with Golang and Python.</p>
+    <h2>What I have built </h2>
+    <article class="highlight">
+      <h3><a href="https://sala.pm" target="_blank" rel="noopener noreferrer">SALA.PM</a></h3>
+      <p>Sala is an online scratchpad to store something you've copied somewhere that you want to continue with later.
+        Each scratchpad is known as a Board and clips are added to it. Other devices can be added to the board to
+        retrieve add more clips. <br>
+        The backend is written in <strong>Golang</strong> and uses go websocket. The frontend is written in
+        <strong>Typescript</strong> and built using
+        <strong>Vue.js</strong><br><br>
+        <a href="https://sala.pm" target="_blank" rel="noopener noreferrer"> Give it a try >>></a>
+      </p>
+      <img class="preview-img" src="./assets/sala-preview.png" alt="Sala preview">
+    </article>
     <ul class="tools">
       <li>
-        <h3>ReURI</h3>
+        <h3><a href="https://reuri.okello.io" target="_blank" rel="noopener noreferrer">ReURI</a></h3>
         <p>A tool to encode and decode URIs</p>
-        <a href="https://reuri.okello.io" target="_blank" rel="noopener noreferrer">reuri.okello.io</a>
+        <p>It is written in Typescript and built using <strong>Vue.Js</strong></p><br>
+        <a href="https://reuri.okello.io" target="_blank" rel="noopener noreferrer">reuri.okello.io >>></a>
       </li>
       <li>
-        <h3>Link A Stream</h3>
-        <p>Find the stream link for many video platforms</p>
-        <a href="https://linkastream.co" target="_blank" rel="noopener noreferrer">linkastream.co</a>
+        <h3><a href="https://linkastream.co" target="_blank" rel="noopener noreferrer">Link A Stream</a></h3>
+        <p>Link A Stream has shut down, but it was a tool to get the direct stream url for almost any video platform</p>
+        <p>It was written in javascript and built using Node.js with Express.js</p><br>
+        <a href="https://github.com/keystroke3/linkastream" target="_blank" rel="noopener noreferrer">Look at the code
+          >>></a>
       </li>
     </ul>
-    <h2>Socials</h2>
+    <h2>My Blog</h2>
+    <p id="blogtext">
+      I have recently created a blog in Hugo called Recursive Heuristics.<br>
+      It is still work in progress, but you can read a
+      few of the articles I have written there.
+      I plan on adding more content to it like my build logs for things I am working on, some interesting finds on the
+      internet, and personal opinions.
+      you can have a look at <br>
+      <a id="bloglink" href="https://blog.okello.io" target="_blank" rel="noopener noreferrer">blog.okello.io >>></a>
+    </p>
+    <h2>How to reach me</h2>
     <ul class="socials">
+      <li>
+        <p>LinkedIn</p>
+        <a href="https://www.linkedin.com/in/teddyokello/" target="_blank" rel="noopener noreferrer">/in/teddyokello</a>
+      </li>
       <li>
         <p>Github</p>
         <a href="https://github.com/keystroke3" target="_blank" rel="noopener noreferrer">@keystroke3</a>
@@ -26,8 +57,9 @@
         <a href="https://twitter.com/keystroke_3" target="_blank" rel="noopener noreferrer">@keystroke_3</a>
       </li>
       <li>
-        <p>My Dev Blog</p>
-        <a href="https://dev.to/keystroke3" target="_blank" rel="noopener noreferrer">Dev.to/keystroke3</a>
+        <p>Fediverse</p>
+        <a href="https://mastodon.social/@SiliconRaven" target="_blank"
+          rel="noopener noreferrer">@SiliconRaven@mastodon.social</a>
       </li>
       <li>
         <p>Email</p>
@@ -38,38 +70,18 @@
 </template>
 
 <style scoped lang="scss">
-.main {
-  width: min(65vw, 70rem);
-  // margin: auto;
-}
-
-.sep {
-  width: inherit;
-  border-bottom: 1px solid var(--color-fg);
-}
-
-.tools {
-  li {
-    min-width: 1fr;
-  }
-}
-
-a {
-  color: var(--color-green);
-}
-
 h1 {
   font-weight: 100;
-  font-size: 4.7rem;
+  font-size: clamp(3rem, 4vw, 4.7rem);
   // white-space: nowrap;
 }
 
 h2 {
   width: inherit;
-  border-bottom: 1px solid hsl(240, 1.6%, 88.0%, 40%);
+  border-bottom: 1px solid var(--color-fg);
   font-weight: 300;
   font-size: 2rem;
-  margin-top: 2rem;
+  margin-block: 2rem;
 }
 
 h3 {
@@ -86,10 +98,63 @@ ul {
 }
 
 li {
-  font-size: 1.2rem;
   font-weight: 300;
   text-align: start;
   flex: 1;
   margin-block: 5%;
+}
+
+.main {
+  width: clamp(300px, 65vw, 70rem);
+  // margin: auto;
+}
+
+.sep {
+  width: inherit;
+  border-bottom: 1px solid var(--color-fg);
+}
+
+.tools {
+  li {
+    min-width: 300px;
+    background-color: var(--color-bg-alt);
+    padding: 2%;
+    border-radius: 10px;
+
+  }
+}
+
+.highlight {
+  min-width: max(300px, 100%);
+  border-block: 2px solid var(--color-orange);
+  border-radius: 15px;
+  margin-bottom: 5%;
+  background-color: var(--color-bg-alt);
+  text-align: start;
+
+  h3 {
+    text-align: center;
+    font-size: 2rem;
+    font-style: bold;
+  }
+
+  p {
+    padding: 2%;
+  }
+
+
+}
+
+.preview-img {
+  max-width: 100%;
+  border-radius: inherit;
+}
+
+#bloglink {
+  font-size: 1.5rem;
+}
+
+#blogtext {
+  text-align: start;
 }
 </style>
